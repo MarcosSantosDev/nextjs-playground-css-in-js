ec37c34bdead43b62129f5e37345485525a5dfc4
MarcosSantosDev <marcosadriano034@gmail.com>
2023-06-06 22:35:33 -0300

    fix(commands):  update cypress tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,26 +1,26 @@
 Cypress.Commands.add('checkTheDashboardPageInPtBrLanguage', () => {
-  cy.title().should('eq', 'LocX | Dashboard');
+  cy.title().should('eq', 'Template | Dashboard');
   cy.contains('Alternador de localidade').should('be.visible');
   cy.contains('Alternar para en').should('be.visible');
   cy.contains('Conteudo em português').should('be.visible');
 });
 
 Cypress.Commands.add('checkTheDashboardPageInEnUsLanguage', () => {
-  cy.title().should('eq', 'LocX | Dashboard');
+  cy.title().should('eq', 'Template | Dashboard');
   cy.contains('Locale switcher').should('be.visible');
   cy.contains('Switch to pt').should('be.visible');
   cy.contains('Content in English').should('be.visible');
 });
 
 Cypress.Commands.add('checkTheSignInPageInPtBrLanguage', () => {
-  cy.title().should('eq', 'LocX | Entrar');
+  cy.title().should('eq', 'Template | Entrar');
   cy.contains('Alternador de localidade').should('be.visible');
   cy.contains('Alternar para en').should('be.visible');
   cy.contains('Conteudo em português').should('be.visible');
 });
 
 Cypress.Commands.add('checkTheSignInPageInEnUsLanguage', () => {
-  cy.title().should('eq', 'LocX | SignIn');
+  cy.title().should('eq', 'Template | SignIn');
   cy.contains('Locale switcher').should('be.visible');
   cy.contains('Switch to pt').should('be.visible');
   cy.contains('Content in English').should('be.visible');
